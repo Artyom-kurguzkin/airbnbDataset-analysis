65fc0b22733969e9eebe39b48c2de49fd957e1bd
Lance Feldman <lance.a.b.feldman@gmail.com>
2023-05-03 09:52:48 +0930

    converting data to a tibble
    still cant export to csv/tsv yet

--- part-01/src/fetch-data.R
+++ part-01/src/fetch-data.R
@@ -10,6 +10,17 @@ connection_string = "mongodb+srv://feld0017:BJrrqJpRLkmh43jG@comp2031.kovjwrp.mo
 # Load the data from mongodb.
 data = mongo(collection = "listingsAndReviews", db = "sample_airbnb", url = connection_string)
 
-# Export the data to a binary json (bson) file.
+# OPTIONAL: Export the data to a binary json (bson) file.
 data$export(file("part-01/dat/data.bson"), bson = TRUE)
+
+# Convert the data to a Tibble:
+# Install Tidyverse
+install.packages("tidyverse")
+library(tidyverse)
+
+# Make the Tibble
+data_tb = as_tibble(data$aggregate())
+
+# This doesn't work, because of the columns have multiple entries? Convert them to vectors?
+write_tsv(data_tb, file="part-01/dat/data.csv",)
 
